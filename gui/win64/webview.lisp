ed1888c00ee2a7da61c9fab3efb04bfa9520fd54
mikel evins <mikel@evins.net>
2023-03-26 16:37:41 -0500

    make webviewlisp.exe load wikipedia and process events

--- gui/win64/webview.lisp
+++ gui/win64/webview.lisp
@@ -17,9 +17,37 @@
     (w :int)
     (p :pointer)))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (cffi::defcfun (webview_set_title "webview_set_title" :library weblib)(:pointer :void)
+    (w :pointer)
+    (title :string)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (cffi::defcfun (webview_set_size "webview_set_size" :library weblib)(:pointer :void)
+    (w :pointer)
+    (width :int)
+    (height :int)
+    (hints :int)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (cffi::defcfun (webview_navigate "webview_navigate" :library weblib)(:pointer :void)
+    (w :pointer)
+    (url :string)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (cffi::defcfun (webview_run "webview_run" :library weblib)(:pointer :void)
+    (w :pointer)))
+
+(defparameter +webview-hint-none+ 0) ;; Width and height are default size
+(defparameter +webview_hint_min+ 1)  ;; Width and height are minimum bounds
+(defparameter +webview_hint_max+ 2)  ;; Width and height are maximum bounds
+(defparameter +webview_hint_fixed+ 3);; Window size can not be changed by a user
+
 (save-lisp-and-die "webviewlisp.exe"
                    :toplevel (lambda (&rest ignore)
-                               (format t "~%hello~%")
-                               (webview_create 0 (cffi:null-pointer))
-                               (format t "~%goodbye~%"))
+                               (let ((w (webview_create 0 (cffi:null-pointer))))
+                                 (webview_set_title w "webview")
+                                 (webview_set_size w 600 400 +webview_hint_min+)
+                                 (webview_navigate w "https://en.m.wikipedia.org/wiki/Main_Page")
+                                 (webview_run w)))
                    :executable t )
