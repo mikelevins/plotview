fae762229e8ab5cbfc934663c84800a8532b89d1
mikel evins <mikel@evins.net>
2023-06-11 14:17:20 -0500

    use singleton class to keep server tidy across restarts

--- src/plotview.lisp
+++ src/plotview.lisp
@@ -16,8 +16,18 @@
 (defun the-plotview-server ()
   (make-instance 'plotview-server))
 
+(defun start-server ()
+  (hunchentoot:start (the-plotview-server))
+  (the-plotview-server))
+
+(defun stop-server ()
+  (hunchentoot:stop (the-plotview-server))
+  (the-plotview-server))
+
 (defun reset-the-plotview-server ()
-  (reset-singleton-class (find-class 'plotview-server)))
+  (hunchentoot:stop (the-plotview-server))
+  (reset-singleton-class (find-class 'plotview-server))
+  (the-plotview-server))
 
 #+nil (the-plotview-server)
 #+nil (reset-the-plotview-server)
